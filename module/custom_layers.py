8b0d8ec08591a03fdf1483ddad223954a0df17f5
Pluto <thiece@live.com>
2023-02-27 20:08:49 +0800

    增加SPD方法
    space_to_depth

--- module/custom_layers.py
+++ module/custom_layers.py
@@ -68,3 +68,13 @@ class DetectHead(nn.Module):
         cls = self.softmax(self.cls_layers(x))
 
         return torch.cat((obj, reg, cls), dim =1)
+
+ #SPD space_to_depth
+class space_to_depth(nn.Module):
+    # Changing the dimension of the Tensor
+    def __init__(self, dimension=1):
+        super().__init__()
+        self.d = dimension
+
+    def forward(self, x):
+         return torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
