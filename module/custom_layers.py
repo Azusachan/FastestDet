83148a4b22a0fcaa5d60ee4a82df008c437bd770
Pluto <thiece@live.com>
2023-02-28 10:32:20 +0800

    SPPFCSPC下Conv改为nn.Conv2d

--- module/custom_layers.py
+++ module/custom_layers.py
@@ -30,14 +30,14 @@ class SPPFCSPC(nn.Module):
     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=5):
         super(SPPFCSPC, self).__init__()
         c_ = int(2 * c2 * e)  # hidden channels
-        self.cv1 = Conv(c1, c_, 1, 1)
-        self.cv2 = Conv(c1, c_, 1, 1)
-        self.cv3 = Conv(c_, c_, 3, 1)
-        self.cv4 = Conv(c_, c_, 1, 1)
+        self.cv1 = nn.Conv2d(c1, c_, 1, 1)
+        self.cv2 = nn.Conv2d(c1, c_, 1, 1)
+        self.cv3 = nn.Conv2d(c_, c_, 3, 1)
+        self.cv4 = nn.Conv2d(c_, c_, 1, 1)
         self.m = nn.MaxPool2d(kernel_size=k, stride=1, padding=k // 2)
-        self.cv5 = Conv(4 * c_, c_, 1, 1)
-        self.cv6 = Conv(c_, c_, 3, 1)
-        self.cv7 = Conv(2 * c_, c2, 1, 1)
+        self.cv5 = nn.Conv2d(4 * c_, c_, 1, 1)
+        self.cv6 = nn.Conv2d(c_, c_, 3, 1)
+        self.cv7 = nn.Conv2d(2 * c_, c2, 1, 1)
 
     def forward(self, x):
         x1 = self.cv4(self.cv3(self.cv1(x)))
